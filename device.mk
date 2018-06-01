e44f32cd522fc66c7757fb395b87fcdc51ea3ef0
merothh <merothh@gmail.com>
2018-06-01 19:07:09 +0000

    whyred: nuke overlay-lineage
     * also move the stuff we need

--- device.mk
+++ device.mk
@@ -26,8 +26,7 @@ $(call inherit-product, vendor/xiaomi/whyred/whyred-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
